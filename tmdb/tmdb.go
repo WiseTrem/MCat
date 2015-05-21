584a9a639dab5cd87be4b2aafc33f40a9060a0fb
Gleb Shepelyov <shepelyov.g@gmail.com>
2015-05-21 12:00:42 +0300

    New download(), exist(), Fatal() changed 2 Print()

--- tmdb/tmdb.go
+++ tmdb/tmdb.go
@@ -3,10 +3,12 @@ package tmdb
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 )
 
 type jsonSearch struct {
@@ -65,7 +67,7 @@ func (j *JsonCast) jsonCastDecode(r []byte) {
 	}
 }
 
-func Request(r string) []byte {
+func request(r string) []byte {
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", r, nil)
 	resp, err := client.Do(req)
@@ -77,16 +79,48 @@ func Request(r string) []byte {
 	return bodyByte
 }
 
+func exist(path string) bool {
+	_, err := os.Stat(path)
+	if err == nil {
+		return true
+	}
+	if os.IsNotExist(err) {
+		return false
+	}
+	fmt.Print(err)
+	return false
+}
+
+func download(ji *JsonInfo) {
+	path := ji.Poster_path
+	url := fmt.Sprint("http://image.tmdb.org/t/p/w500", path)
+	home := os.Getenv("HOME")
+	os.MkdirAll((home + "/.MCat/posters"), 0777)
+	if !exist((home + "/.MCat/posters") + path) {
+		out, err := os.Create((home + "/.MCat/posters") + path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer out.Close()
+		resp, err := http.Get(url)
+		defer resp.Body.Close()
+		_, errCopy := io.Copy(out, resp.Body)
+		if errCopy != nil {
+			fmt.Println(errCopy)
+		}
+	}
+}
+
 func GetInfo(s string) (*JsonInfo, *JsonCast, error) {
 	urlSearch, err := url.Parse("http://api.themoviedb.org/3/search/movie?query=template&api_key=a5c697bcbfb66710e125f672937c78c0")
 	if err != nil {
-		log.Fatal(err)
+		return nil, nil, err
 	}
 	q := urlSearch.Query()
 	q.Set("query", s)
 	urlSearch.RawQuery = q.Encode()
 
-	respSearch := Request(urlSearch.String())
+	respSearch := request(urlSearch.String())
 
 	j := &jsonSearch{}
 	if n := j.jsonSearchDecode(respSearch); n == 0 {
@@ -98,20 +132,22 @@ func GetInfo(s string) (*JsonInfo, *JsonCast, error) {
 
 	urlGet, err := url.Parse("http://api.themoviedb.org/template?api_key=a5c697bcbfb66710e125f672937c78c0&language=ru")
 	if err != nil {
-		log.Fatal(err)
+		return nil, nil, err
 	}
 	urlGet.Path = fmt.Sprint("3/movie/", id)
-	respGet := Request(urlGet.String())
+	respGet := request(urlGet.String())
 
 	ji := &JsonInfo{}
 	ji.jsonInfoDecode(respGet)
 
+	go download(ji)
+
 	urlCast, err := url.Parse("http://api.themoviedb.org/template?api_key=a5c697bcbfb66710e125f672937c78c0&language=ru")
 	if err != nil {
-		log.Fatal(err)
+		return nil, nil, err
 	}
 	urlCast.Path = fmt.Sprint("3/movie/", id, "/credits")
-	respCast := Request(urlCast.String())
+	respCast := request(urlCast.String())
 
 	jc := &JsonCast{}
 	jc.jsonCastDecode(respCast)
